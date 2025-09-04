90954d4eaeb206a5ad5558ece309ccc5f9cefca1
Mehrnoosh H. Kashani <59601818+mehrnoosh-hk@users.noreply.github.com>
2025-09-04 23:22:32 +0330

    refactor(repository): update UserRepository interface to decouple from sqlc implementation

--- services/api/internal/db/repository/repo_interface.go
+++ services/api/internal/db/repository/repo_interface.go
@@ -2,9 +2,10 @@ package repository
 
 import (
 	"context"
-	sqlc "rdl-api/internal/db/sqlc"
 
-	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/google/uuid"
+	
+	models "rdl-api/internal/domain/models"
 )
 
 // Repository interface
@@ -21,16 +22,21 @@ type HealthRepository interface {
 
 // UserRepository defines the interface for user-related database operations
 type UserRepository interface {
-	CreateUser(ctx context.Context, arg sqlc.CreateUserParams) (sqlc.User, error)
-	DeleteUser(ctx context.Context, id pgtype.UUID) (int64, error)
-	GetAllUsers(ctx context.Context) ([]sqlc.User, error)
+	CreateUser(ctx context.Context, arg models.CreateUserParams) (models.User, error)
+	DeleteUser(ctx context.Context, id uuid.UUID) (int64, error)
+	GetAllUsers(ctx context.Context) ([]models.User, error)
 	// users table queries
-	GetUserByEmail(ctx context.Context, email string) (sqlc.User, error)
-	GetUserById(ctx context.Context, id pgtype.UUID) (sqlc.User, error)
-	UpdateUser(ctx context.Context, arg sqlc.UpdateUserParams) (sqlc.User, error)
+	GetUserByEmail(ctx context.Context, email string) (models.User, error)
+	GetUserById(ctx context.Context, id uuid.UUID) (models.User, error)
+	UpdateUser(ctx context.Context, arg models.UpdateUserParams) (models.User, error)
 }
 
 // Database abstracts the database connection
 type Database interface {
 	Ping(ctx context.Context) error
+}
+
+type UUID struct {
+	Bytes [16]byte
+	Valid bool
 }
