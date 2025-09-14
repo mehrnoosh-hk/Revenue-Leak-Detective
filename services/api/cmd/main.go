7665ca5cc53450857c80297ab14f73dafa05bd6b
Mehrnoosh H. Kashani <59601818+mehrnoosh-hk@users.noreply.github.com>
2025-09-15 01:23:22 +0330

    refactor(api): implement command line flag parsing and handling for version and health checks

--- services/api/cmd/main.go
+++ services/api/cmd/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
-	"fmt"
 	"log/slog"
 	"os"
 	"rdl-api/config"
@@ -12,34 +10,18 @@ import (
 
 func main() {
 	// Parse command line flags
-	versionFlag := flag.Bool("version", false, "Show version information")
-	healthFlag := flag.Bool("health", false, "Run health check and exit")
-	envFileFlag := flag.String("env-file", "", "Path to environment file (required)")
-	flag.Parse()
-
-	if *envFileFlag == "" {
-		slog.Warn("No env file provided, using only environment variables or fallbacks to defaults(development only)")
-	}
+	flags := parseFlags()
 
 	// Load configuration
-	cfg, err := config.LoadConfig(*envFileFlag)
+	cfg, err := config.LoadConfig(flags.EnvFile)
 	if err != nil {
-		slog.Error("Failed to load configuration", "error", err)
+		slog.Error("Failed to load configuration", "error", err, "env_file", flags.EnvFile)
 		os.Exit(1)
 	}
 
-	// Handle version flag
-	if *versionFlag {
-		handleVersionFlag(cfg)
-	}
-
-	// Log startup information
 	slog.Info("Starting Revenue Leak Detective API")
 
-	// Move to app package
-	// cfg.PrintBuildInfo(logger)
-	// cfg.PrintEffectiveConfig(logger)
-
+	// Create application
 	application, err := app.New(cfg)
 	if err != nil {
 		slog.Error("Failed to create application", "error", err)
@@ -48,26 +30,11 @@ func main() {
 
 	ctx := context.Background()
 
-	if *healthFlag {
-		err := application.CheckReadiness(ctx)
-		if err != nil {
-			slog.Error("Health check failed", "error", err)
-			os.Exit(1)
-		}
-		fmt.Println("Health check: OK")
-		os.Exit(0)
-	}
+	// Handle flags
+	flags.HandleFlags(ctx, application, cfg)
 
 	if err := application.StartUp(ctx); err != nil {
 		slog.Error("Server failed to start", "error", err)
 		os.Exit(1)
 	}
-}
-
-func handleVersionFlag(cfg *config.Config) {
-	fmt.Printf("Revenue Leak Detective API\n")
-	fmt.Printf("Version: %s", cfg.BuildInfo.GIT_TAG)
-	fmt.Printf("Commit: %s", cfg.BuildInfo.GIT_COMMIT_FULL)
-	fmt.Printf("Built: %s", cfg.BuildInfo.BUILD_TIMESTAMP)
-	os.Exit(0)
 }
